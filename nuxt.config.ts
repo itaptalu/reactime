e1cca1625a715848316d43ed63eb2dc98b7c1a47
itaptalu <itaptalu@gmail.com>
2026-08-30 03:00:17 +0700

    init webmanifest and icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,35 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
     ssr: false,
 
+    app: {
+        head: {
+            title: 'reactime',
+            link: [
+                {
+                    rel: 'apple-touch-icon',
+                    sizes: '180x180',
+                    href: '/apple-touch-icon.png',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '32x32',
+                    href: '/favicon-32x32.png',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '16x16',
+                    href: '/favicon-16x16.png',
+                },
+                {
+                    rel: 'manifest',
+                    href: '/site.webmanifest',
+                },
+            ],
+        },
+    },
+
     modules: ['@nuxt/icon', '@nuxtjs/tailwindcss', '@nuxt/eslint', '@vueuse/nuxt'],
 
     icon: {
