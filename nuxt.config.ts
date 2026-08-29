25b81a83656442dc8152cb4188b7c2a3a959c670
itaptalu <itaptalu@gmail.com>
2026-08-30 04:24:08 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,10 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
     ssr: true,
 
+    nitro: {
+        preset: 'static',
+    },
+
     app: {
         head: {
             title: 'reactime',
