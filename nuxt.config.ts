ac0089b99f87cf4e8d24aa5e7a5c68df4c532b56
itaptalu <itaptalu@gmail.com>
2026-08-30 04:56:17 +0700

    update config for vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,7 @@ export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
     ssr: true,
-
-    nitro: {
-        preset: 'static',
-    },
-
     app: {
-        baseURL: '/reactime/',
         head: {
             title: 'reactime',
             link: [
