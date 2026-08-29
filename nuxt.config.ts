323e29b2b5b8a1a00c64a6b3bfa8450c17db7762
itaptalu <itaptalu@gmail.com>
2026-08-30 04:09:58 +0700

    add output

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
-    ssr: false,
+    ssr: true,
+
+    nitro: {
+        preset: 'github-pages',
+    },
 
     app: {
         head: {
